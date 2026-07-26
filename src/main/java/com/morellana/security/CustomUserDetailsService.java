5cb5dbb4960e6661e1e7559bf0dd58d20f6ed050
mauricio orellana <mau.orellana9191@gmail.com>
2026-07-25 20:12:55 -0600

    Add UserRepository and CustomUserDetailsService

---
+++ src/main/java/com/morellana/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.morellana.security;
+
+import com.morellana.model.User;
+import com.morellana.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("User with email " + username + "not found")
+        );
+
+        return new CustomUserDetails(user);
+    }
+}
